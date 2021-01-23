47696c22209bcd9ac58720f879320bbc6cd97efb
keno1907 <63511659+keno1907@users.noreply.github.com>
2021-01-23 18:53:35 +0300

    23 Ocak

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+        K[] tempKeys;
+        V[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key,V value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+            keys = new K[keys.Length+1];
+            values = new V[values.Length+1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        //Keys
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+
+        //Values
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+        //Count
+        public int Count
+        {
+            get { return keys.Length; }
+
+        }
+    }
+}
--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary
 {
@@ -6,7 +7,35 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDictionary<string, int> cities = new MyDictionary<string, int>();
+            cities.Add("Istanbul",34);
+            cities.Add("Ankara", 06);
+            cities.Add("Adana", 01);
+            cities.Add("Trabzon", 61);
+            cities.Add("Mersin", 33);
+
+
+            Console.WriteLine("Cities:");
+            Console.WriteLine("----------------");
+            foreach (var city in cities.Keys)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("----------------");
+
+            Console.WriteLine("Plaka Id:");
+            Console.WriteLine("----------------");
+            foreach (var plaka in cities.Values)
+            {
+                Console.WriteLine(plaka);
+            }
+
+            Console.WriteLine("----------------");
+
+            Console.WriteLine(cities.Count);
+
+            Console.ReadLine();
         }
     }
 }
